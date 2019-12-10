cda6183ae3c14f986969792a67599f25e9e5e859
Yuriy Yurev <iuriev-iurii1@mail.ru>
2019-12-10 23:54:30 +0700

    f

--- src/modules/showRepairAndPrivacy.js
+++ src/modules/showRepairAndPrivacy.js
@@ -2,28 +2,19 @@ const showRepairAndPrivacy = () => {
   const popupRepairTypes = document.querySelector('.popup-repair-types'),
         popupPrivacy = document.querySelector('.popup-privacy');
 
-  const showType = (selector, types, btnClose, target) => {
+  const showType = (selector, types, target) => {
     if (target.closest(selector)) {
       types.style.visibility = 'visible';
-    }
-    if (target.closest(btnClose)) {
-      types.style.visibility = '';
-      const itemNav = document.querySelectorAll('.popup-repair-types-nav__item'),
-      tableItem = document.querySelectorAll('.popup-repair-types-content-table__list');
-      for (let i = 0; i < itemNav.length; i ++) {
-        itemNav[i].remove();
-        tableItem[i].remove();
-      }
     }
   };
 
   document.body.addEventListener('click', (event) => {
     let target = event.target;
-    showType('.link-list', popupRepairTypes, '.close', target);
-    showType('.link-privacy', popupPrivacy, '.close', target);
+    showType('.link-list', popupRepairTypes, target);
+    showType('.link-privacy', popupPrivacy, target);
   });
   popupRepairTypes.addEventListener('click', (event) => {
-    if (!event.target.closest('.popup-dialog-repair-types')) {
+    if (!event.target.closest('.popup-dialog-repair-types') || event.target.closest('.close')) {
       popupRepairTypes.style.visibility = '';
       const itemNav = document.querySelectorAll('.popup-repair-types-nav__item'),
       tableItem = document.querySelectorAll('.popup-repair-types-content-table__list');
@@ -31,6 +22,12 @@ const showRepairAndPrivacy = () => {
         itemNav[i].remove();
         tableItem[i].remove();
       }
+    }
+  });
+  popupPrivacy.addEventListener('click', (event) => {
+    let target = event.target;
+    if (!target.closest('.popup-dialog-privacy') || target.closest('.close')) {
+      popupPrivacy.style.visibility = '';
     }
   });
 };
